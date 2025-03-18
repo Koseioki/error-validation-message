9ebd6743542a5f3ecbd739e05323719ceebce88b
Kosei Oki <107920132+Koseioki@users.noreply.github.com>
2025-03-18 15:37:57 +0100

    added all the inputs

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ document.getElementById('form').addEventListener('submit', function(e) {
   e.preventDefault();
   var textInput = document.getElementById('textinput').value;
   var textInputError = document.getElementById('textinputError');
+  var textareaError = document.getElementById('textareaError');
+  var radioError = document.getElementById('radioError');
+  var checkboxError = document.getElementById('checkboxError');
 
     textInputError.innerHTML  = '<img src="./error.svg" alt="" /> Error: I said anything but there is an error';
-
-    var radioError = document.getElementById('radioError');
+    textareaError.innerHTML = '<img src="./error.svg" alt="" /> Error: I said anything but there is an error';
     radioError.innerHTML = '<img src="./error.svg" alt="" /> Error: I said any of them but there is an error';
-
+    checkboxError.innerHTML = '<img src="./error.svg" alt="" /> Error: I said any of them but there is an error';
 
     // set focus to #alert
     var alert = document.getElementById('alert');
