8064e955e05e09f3ff1e0c83af786de381a8501b
Kosei Oki <107920132+Koseioki@users.noreply.github.com>
2025-03-21 10:04:05 +0100

    changed the texts

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-// after submitting the form, add text in #textinputError
-// if the input is empty, add text "Please enter your name"
-
 document.getElementById('form').addEventListener('submit', function(e) {
   e.preventDefault();
-  var textInput = document.getElementById('textinput').value;
-  var textInputError = document.getElementById('textinputError');
-  var textareaError = document.getElementById('textareaError');
+  var fullnameError = document.getElementById('fullnameError');
+  var requirementsError = document.getElementById('requirementsError');
   var radioError = document.getElementById('radioError');
   var checkboxError = document.getElementById('checkboxError');
 
-    textInputError.innerHTML  = '<img src="./error.svg" alt="" /> Error: I said anything but there is an error';
-    textareaError.innerHTML = '<img src="./error.svg" alt="" /> Error: I said anything but there is an error';
-    radioError.innerHTML = '<img src="./error.svg" alt="" /> Error: I said any of them but there is an error';
-    checkboxError.innerHTML = '<img src="./error.svg" alt="" /> Error: I said any of them but there is an error';
+    fullnameError.innerHTML  = '<img src="./error.svg" alt="" /> Error: Enter your full name.';
+    requirementsError.innerHTML = '<img src="./error.svg" alt="" /> Error: Provide any special dietary requirements or leave the field blank if none.';
+    radioError.innerHTML = '<img src="./error.svg" alt="" /> Error: Select your preferred meal plan.';
+    checkboxError.innerHTML = '<img src="./error.svg" alt="" /> Error: Select at least one additional preference, or uncheck if none apply.';
 
     // set focus to #alert
     var alert = document.getElementById('alert');
     // remove attribute hidden	
     alert.removeAttribute('hidden');
-    alert.textContent = 'There are 2 errors in this form';
+    alert.textContent = 'There are 4 errors in this form';
     alert.focus();
 });
