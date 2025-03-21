232444893e97d7ccf87d493045d5671680ca1994
Kosei Oki <107920132+Koseioki@users.noreply.github.com>
2025-03-21 12:50:17 +0100

    added an error list

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ document.getElementById('form').addEventListener('submit', function(e) {
   var radioError = document.getElementById('radioError');
   var checkboxError = document.getElementById('checkboxError');
 
-    fullnameError.innerHTML  = '<img src="./error.svg" alt="" /> Error: Enter your full name.';
-    requirementsError.innerHTML = '<img src="./error.svg" alt="" /> Error: Provide any special dietary requirements or leave the field blank if none.';
-    radioError.innerHTML = '<img src="./error.svg" alt="" /> Error: Select your preferred meal plan.';
-    checkboxError.innerHTML = '<img src="./error.svg" alt="" /> Error: Select at least one additional preference, or uncheck if none apply.';
+    fullnameError.innerHTML  = '<img src="./error.svg" alt="" /> Enter your full name.';
+    requirementsError.innerHTML = '<img src="./error.svg" alt="" /> Provide any special dietary requirements or leave the field blank if none.';
+    radioError.innerHTML = '<img src="./error.svg" alt="" /> Select your preferred meal plan.';
+    checkboxError.innerHTML = '<img src="./error.svg" alt="" /> Select at least one additional preference, or uncheck if none apply.';
 
     // set focus to #alert
     var alert = document.getElementById('alert');
     // remove attribute hidden	
     alert.removeAttribute('hidden');
-    alert.textContent = 'There are 4 errors in this form';
+    alert.innerHTML = '<p>There are 4 errors in this form</p> <ul> <li> <a href="#fullname">' + fullnameError.textContent + '</a> </li> <li> <a href="#requirements">' + requirementsError.textContent + '</a> </li> <li> <a href="#radio">' + radioError.textContent + '</a> </li> <li> <a href="#checkbox">' + checkboxError.textContent + '</a> </li> </ul>';
     alert.focus();
 });
